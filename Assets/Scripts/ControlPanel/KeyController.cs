2e9d25dc09145acd3b66bed67ec1dc86f73133de
joezbay <GitHub@oezbay.rocks>
2025-09-20 02:04:09 +0200

    added scripts for basic eventhandling on control panel

--- Assets/Scripts/ControlPanel/KeyController.cs
+++ Assets/Scripts/ControlPanel/KeyController.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    public Interactable Interactable;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Interactable.OnUsed.AddListener(OnUsed);
     }
 
+    private void OnUsed()
+    {
+        Debug.Log(gameObject.name + " OnUsed in KeyController");
+    }
     // Update is called once per frame
     void Update()
     {
