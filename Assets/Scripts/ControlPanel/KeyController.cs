6a42f36e4d9205ef85bf445a28b5588455c7e784
joezbay <GitHub@oezbay.rocks>
2025-09-20 20:18:51 +0200

    ControlPanel erweitert mit Prüfung auf korrekte Sequenz und invoken von Events

--- Assets/Scripts/ControlPanel/KeyController.cs
+++ Assets/Scripts/ControlPanel/KeyController.cs
@@ -3,19 +3,35 @@ using UnityEngine;
 public class KeyController : MonoBehaviour
 {
     public Interactable Interactable;
+    private KeyPad kp;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Interactable.OnUsed.AddListener(OnUsed);
+        kp = gameObject.GetComponentInParent<KeyPad>();
     }
 
-    private void OnUsed()
-    {
-        Debug.Log(gameObject.name + " OnUsed in KeyController");
-    }
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    private void OnUsed()
+    {
+        kp.OnKeyInput(gameObject.name);
+    }
+
+    private void OnHoverStated()
+    {
+        //use for Highlight shader
+    }
+
+    private void OnHoverEnded()
+    {
+
     }
 }
