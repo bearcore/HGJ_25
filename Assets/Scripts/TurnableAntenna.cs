a9982a1227d95aac9788d98c202e1dbea058d6c6
bear <dev@anduogames.com>
2025-09-21 13:16:14 +0200

    polish polish polish polish polish polish polish polish polish

--- Assets/Scripts/TurnableAntenna.cs
+++ Assets/Scripts/TurnableAntenna.cs
@@ -8,6 +8,7 @@ public class TurnableAntenna : MonoBehaviour
     public Interactable Interactable;
     public AudioTest AudioTest;
     public List<AntennaTarget> Targets;
+    public List<GameObject> Indicators;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,11 +18,19 @@ public class TurnableAntenna : MonoBehaviour
 
     private void OnUsed()
     {
+        var anyMatch = false;
         transform.eulerAngles = transform.eulerAngles + new Vector3(0f, RotationPerPress);
         foreach (var target in Targets)
         {
             var match = Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, target.TargetAngle)) < 25f;
             AudioTest.Frequencies[target.UnlockIndex].IsActive = match;
+
+            if (match) anyMatch = true;
+        }
+
+        foreach (var indicator in Indicators)
+        {
+            indicator.SetActive(anyMatch);
         }
     }
 
